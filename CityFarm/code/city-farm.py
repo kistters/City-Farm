b240a16e770979482e0723ebed4f02763600bbae
kisters <rafael.kisters@fcamara.com.br>
2018-03-21 01:34:46 -0300

    understand cookie functions

--- CityFarm/code/city-farm.py
+++ CityFarm/code/city-farm.py
@@ -1,19 +1,27 @@
-from flask import Flask
+from flask import Flask, jsonify, request, make_response
+from tasks import plant, eat
 import random
 
 
 app = Flask(__name__)
 
 @app.route('/')
-def tasks_list():
-    if bool(random.getrandbits(1)):
-        return "Hello Farmer!"
+def home():
+	response = make_response(jsonify({'ip': request.remote_addr, 'cookie': request.cookies.get('cityfarm')}))
+	response.set_cookie('cityfarm',value='Farmer')
+	return response, 200
 
-    return "Hello Citizen!"
 
 @app.route('/work', methods=['POST'])
-def add_task():
-    return "Go go!"
+def work():
+	if bool(random.getrandbits(1)):
+		eat.apply_async(("wheat",), queue='eating', serializer='json')
+		eat.apply_async(("corn",), queue='eating', serializer='json')
+		return jsonify({'ip': request.remote_addr, 'eat':'citizen'}), 200
+
+	plant.apply_async(["corn"], queue='planting', serializer='json')
+	plant.apply_async(["wheat"], queue='planting', serializer='json')
+	return jsonify({'ip': request.remote_addr,'plan':'farmer'}), 200
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
--- untitled.sh
+++ untitled.sh
@@ -34,6 +34,7 @@ ex-next() {
 }
 
 docker rm -f $(docker ps -a -q)
+docker-compose build
 docker-compose up -d rabbitmq redis
 
 ex-next
@@ -42,7 +43,7 @@ gnome-terminal --geometry 80x10+999 -e 'sh -c "docker-compose run --rm worker sh
 
 ex-next
 
-gnome-terminal --geometry 80x10-999 -e 'sh -c "docker exec -it db_redis bash /scripts/getter-redis.sh"'
+gnome-terminal --geometry 39x9+-9+-8 -e 'sh -c "docker exec -it db_redis bash /scripts/getter-redis.sh"'
 
 ex-next
 
@@ -50,5 +51,8 @@ gnome-terminal -e 'sh -c "docker-compose run --rm publisher python farmer.py"'
 
 ex-next 
 
-gnome-terminal --geometry 80x10+999+999 -e 'sh -c "docker stats $(docker ps --format={{.Names}})"'
-gnome-terminal -e 'sh -c "docker-compose run --rm publisher sh"'
+gnome-terminal --geometry 999x10+999+999 -e 'sh -c "docker stats $(docker ps --format={{.Names}})"'
+gnome-terminal --geometry 67x11+-9+262   -e 'sh -c "docker-compose up --remove-orphans  web"'
+gnome-terminal --geometry 67x11-7+267    -e 'sh -c "docker-compose run --rm publisher sh"'
+
+#xwininfo -id $(xprop -root | awk '/_NET_ACTIVE_WINDOW\(WINDOW\)/{print $NF}')
