f2386f971f0967355e3b3a92ccfde11e248d0b34
kisters <rafael.kisters@fcamara.com.br>
2018-03-23 13:38:21 -0300

    removendo .py que não são mais usados

--- CityFarm/code/citizen.py
+++
@@ -1,4 +0,0 @@
-from tasks import eat
-
-eat.apply_async(("corn",), queue='eating', serializer='json')
-eat.apply_async(("wheat",), queue='eating', serializer='json')
--- CityFarm/code/farmer.py
+++
@@ -1,8 +0,0 @@
-from tasks import plant
-
-#plant.apply_async(("corn",), queue='planting', serializer='json')
-#plant.apply_async(("wheat",), queue='planting', serializer='json')
-plant.apply_async(["corn"], queue='planting', serializer='json')
-plant.apply_async(["wheat"], queue='planting', serializer='json')
-
-# for idx, item in enumerate(items):
